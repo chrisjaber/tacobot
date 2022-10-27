72155ecffae6664cf6dd2bad87919a74b44293ff
Chris Jaber <chris.jaber@junkbrands.com>
2022-10-27 15:14:16 -0500

    Update slack.test.js code

--- slack.test.js
+++ slack.test.js
@@ -1,3 +1,22 @@
 const slack = require("./slack.js");
 
-slack.getAllUsers();
+const formatUsers = users => {
+  const members = users.members.filter(user=>!user.deleted);
+  return members.map(member => ({
+    id: member.id,
+    name: member.name
+  }));
+};
+
+slack
+.getAllUsers()
+.then(formatUsers)
+.then((users) => {
+  console.log("POPULATED DB!");
+  console.log(users)
+})
+.catch(e => {
+  console.log("ERROR WHEN POPULATING DB :(");
+  console.log(e);
+});
+

b184102b1ecdc56d3e51f7ce8f912e636ea464f7
Chris Jaber <chris.jaber@junkbrands.com>
2023-01-12 10:20:37 -0600

    add forUser update and Tacobell response

--- bot.js
+++ bot.js
@@ -4,32 +4,33 @@ const DB = require("./db.js");
 const config = require("./config.js");
 
 const forTaco = controller => {
-  controller.hears(":taco:", "ambient", (bot, message) => {
+  controller.hears([":taco:", ":tacobell:"], "ambient", (bot, message) => {
     const id = parser.findID(message.text);
-    const dateStr = new Date().toISOString().slice(0,-5)
+    const dateStr = new Date().toISOString().slice(0, -5);
     if (id !== null) {
       const ids = DB.getIDs();
       const userIndex = ids.indexOf(id);
       const senderIndex = ids.indexOf(message.user);
-      const tacosGiven = parser.countTacos(message.text);
+      const tacobell = message.text.includes(":tacobell:");
+      const tacosGiven = tacobell ? 12 : parser.countTacos(message.text);
       const kingBurrito = message.user === config.kingBurritoId;
-      if ((userIndex > -1 && userIndex !== senderIndex)) {
+      if (userIndex > -1 && userIndex !== senderIndex) {
         const giver = DB.getUser(senderIndex);
-        if (giver.left >= tacosGiven) {
+        if (giver.left >= tacosGiven || (tacobell && giver.left == 5)) {
           // if (config.bannedUsers.includes(id)) {
           //   bot.say({text: `Sorry, <@${id}> has been placed under administrative review and is therefore no longer eligible to receive tacos.`, channel:ids[senderIndex]})
           //   return
           // }
           taco.giveTaco(userIndex, tacosGiven);
-          taco.removeLeft(senderIndex, tacosGiven);
-          const senderName = DB.getUsername(senderIndex)
-          const receiverName = DB.getUsername(userIndex)
-          console.log(`${dateStr}: ${senderName} gave ${tacosGiven} taco${tacosGiven === 1 ? '': 's'} to ${receiverName}`)
-          let msg = `You just received ${tacosGiven} taco${tacosGiven === 1 ? '': 's'} from <@${senderName}>!`
-          bot.say({text: msg, channel:ids[userIndex]})
-          if (!config.silencedUsers.includes(ids[senderIndex])){
-            let senderMsg = `You just gave ${tacosGiven} taco${tacosGiven === 1 ? '': 's'} to <@${receiverName}>!`
-            bot.say({text: senderMsg, channel:ids[senderIndex]})
+          taco.removeLeft(senderIndex, (tacobell ? 5 : tacosGiven));
+          const senderName = DB.getUsername(senderIndex);
+          const receiverName = DB.getUsername(userIndex);
+          console.log(`${dateStr}: ${senderName} gave ${tacosGiven} taco${tacosGiven === 1 ? "" : "s"} to ${receiverName}`);
+          let msg = `You just received ${tacosGiven} taco${tacosGiven === 1 ? "" : "s"} from <@${senderName}>!`;
+          bot.say({ text: msg, channel: ids[userIndex] });
+          if (!config.silencedUsers.includes(ids[senderIndex])) {
+            let senderMsg = `You just gave ${tacosGiven} taco${tacosGiven === 1 ? "" : "s"} to <@${receiverName}>!`;
+            bot.say({ text: senderMsg, channel: ids[senderIndex] });
           }
           // bot.say(`You just received ${tacosGiven} taco${tacosGiven === 1 ? '': 's'} from ${senderName}!`)
           // bot.api.reactions.add(
@@ -45,9 +46,9 @@ const forTaco = controller => {
           //   }
           // );
         } else {
-          console.log(`${dateStr}: ${senderName} tried to give ${tacosGiven} taco${tacosGiven === 1 ? '': 's'} to ${receiverName}, but only had ${giver.left}`)
-          let noTacoMsg = `Sorry <@${message.user}>, you only have ${giver.left} taco${giver.left === 1 ? '': 's'} remaning and you tried to give *${tacosGiven}*`
-          bot.say({text: noTacoMsg, channel:ids[senderIndex]})
+          console.log(`${dateStr}: ${senderName} tried to give ${tacosGiven} taco${tacosGiven === 1 ? "" : "s"} to ${receiverName}, but only had ${giver.left}`);
+          let noTacoMsg = `Sorry <@${message.user}>, you only have ${giver.left} taco${giver.left === 1 ? "" : "s"} remaning and you tried to give *${tacosGiven}*`;
+          bot.say({ text: noTacoMsg, channel: ids[senderIndex] });
         }
       }
     }
@@ -99,7 +100,7 @@ const forReaction = controller => {
 
 const forScore = controller => {
   controller.hears(
-    ["score", "ranking"],
+    ["leaderboard"],
     ["direct_mention", "direct_message"],
     (bot, message) => {
       const users = DB.getUsers();
@@ -113,6 +114,25 @@ const forScore = controller => {
     }
   );
 };
+
+const forUser = controller => {
+  controller.hears(
+    ["score", "ranking"],
+    "direct_message",
+    (bot, message) => {
+      const ids = DB.getIDs();
+      const userIndex = ids.indexOf(message.user);
+      const users = DB.getUsers();
+      const user = DB.getUser(userIndex);
+      const ranked = users.sort((a, b) => b.tacos - a.tacos);
+      // const userRanking = ranked.indexOf(user);
+      const userRanking = users.sort((a, b) => b.tacos - a.tacos).map(DBUser => DBUser.id).indexOf(message.user)+1;
+      bot.reply(message, `You are ranked #${userRanking} with ${user.tacos} taco${user.left === 1 ? '': 's'}`);
+    }
+  );
+};
+
+
 
 const forLeft = controller => {
   controller.hears(
@@ -139,7 +159,9 @@ In the <#C0FQWQXNY> channel, just mention someone and add the :taco: emoji next 
 
 You can find out how many :taco: you have left by direct messaging me \`left\` or \`how many\`.
 
-If you want to see the leaderboard, ask me \`score\` or \`ranking\`
+You can see your ranking and how many tacos you have by asking \`score\` or \`ranking\`.
+
+If you want to see the leaderboard, ask me \`leaderboard\`.
     `
       );
     }
@@ -150,6 +172,7 @@ const listens = controller => {
   forTaco(controller);
   forReaction(controller);
   forScore(controller);
+  forUser(controller);
   forLeft(controller);
   forHelp(controller);
 };
